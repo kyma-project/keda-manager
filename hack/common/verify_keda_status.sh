68df3717bf0a72a31a52915e8cd85319ed1e7497
Marcin Dobrochowski <anoip@o2.pl>
2023-03-14 14:50:57 +0100

    refactor makefiles structure; prepare separation of lifecycle manager

---
+++ hack/common/verify_keda_status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function get_keda_status () {
+	local number=1
+	while [[ $number -le 100 ]] ; do
+		echo ">--> checking keda status #$number"
+		local STATUS=$(kubectl get keda -n kyma-system keda-sample -o jsonpath='{.status.state}')
+		echo "keda status: ${STATUS:='UNKNOWN'}"
+		[[ "$STATUS" == "Ready" ]] && return 0
+		sleep 5
+        	((number = number + 1))
+	done
+
+	kubectl get all --all-namespaces
+	exit 1
+}
+
+get_keda_status
--- hack/common/verify_kyma_status.sh
+++ hack/common/verify_kyma_status.sh
@@ -2,9 +2,9 @@
 
 function get_kyma_status () {
 	local number=1
-	while [[ $number -le 12*3 ]] ; do
+	while [[ $number -le 100 ]] ; do
 		echo ">--> checking kyma status #$number"
-		local STATUS=$(kubectl get keda -n kyma-system keda-sample -o jsonpath='{.status.state}')
+		local STATUS=$(kubectl get kyma -n kyma-system default-kyma -o jsonpath='{.status.state}')
 		echo "kyma status: ${STATUS:='UNKNOWN'}"
 		[[ "$STATUS" == "Ready" ]] && return 0
 		sleep 5
