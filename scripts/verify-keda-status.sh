72b54cbd2be21e3be01df4fe5ff88d132e55f59f
Andrzej Pankowski <andrzej.pankowski@sap.com>
2023-06-07 13:20:22 +0200

    add-sleep

--- scripts/verify-keda-status.sh
+++ scripts/verify-keda-status.sh
@@ -6,6 +6,7 @@ echo "Checking status of POST Jobs for Keda-Manager"
 
 fullstatus=`curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/kyma-project/keda-manager/commits/main/status | head -n 2 `
 
+sleep 10
 echo $fullstatus
 
 if [[ "$fullstatus" == *"success"* ]]; then
