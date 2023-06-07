cc8a030e95ea91ea5f1afedc2eb190795bf57a39
Andrzej Pankowski <andrzej.pankowski@sap.com>
2023-06-07 12:50:42 +0200

    fix_verify

--- scripts/verify-keda-status.sh
+++ scripts/verify-keda-status.sh
@@ -4,10 +4,11 @@ export ACCESS_TOKEN=$1
 
 echo "Checking status of POST Jobs for Keda-Manager"
 
-if curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/kyma-project/keda-manager/commits/main/status | grep -q 'failure\|pending'
-then
-  echo "Jobs failed and/or in progress - check post jobs status"
-  exit 1
-else
+fullstatus=`curl -L -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/kyma-project/keda-manager/commits/main/status | head -n 2 `
+
+if [[ "$fullstatus" == *"success"* ]]; then
   echo "All jobs succeeded"
+else
+  echo "Jobs failed or pending - Check Prow status"
+  exit 1
 fi
