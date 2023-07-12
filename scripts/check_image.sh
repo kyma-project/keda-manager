5646a802c3d2634116a17f8af96048c70a7a5a22
MichalKalke <michal.kalke@sap.com>
2023-07-12 16:12:38 +0200

    changed hash to tag on final versions

--- scripts/check_image.sh
+++ scripts/check_image.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-RAW_EXPECTED_HASH=$(git log main --author "Kyma Bot" --max-count 1 --skip 1 --format=format:%H)
-SHORT_EXPECTED_HASH=${RAW_EXPECTED_HASH:0:8}
+RAW_EXPECTED_TAG=$(git log main --author "Kyma Bot" --max-count 1 --skip 1 --format=format:%H)
+SHORT_EXPECTED_TAG=${RAW_EXPECTED_TAG:0:8}
 DATE="v$(git log main --author "Kyma Bot" --max-count 1 --skip 1 --format=format:%ad --date=format:'%Y%m%d')"
-EXPECTED_HASH="${DATE}-${SHORT_EXPECTED_HASH}"
+EXPECTED_TAG="${DATE}-${SHORT_EXPECTED_TAG}"
 
 IMAGE_TO_CHECK="${1:-europe-docker.pkg.dev/kyma-project/prod/keda-manager}"
-BUMPED_IMAGE_HASH=$(cat sec-scanners-config.yaml | grep "${IMAGE_TO_CHECK}" | cut -d : -f 2)
+BUMPED_IMAGE_TAG=$(cat sec-scanners-config.yaml | grep "${IMAGE_TO_CHECK}" | cut -d : -f 2)
 
-if [[ "$BUMPED_IMAGE_HASH" != "$EXPECTED_HASH" ]]; then
-  echo "Tags are not correct: wanted $EXPECTED_HASH but got $BUMPED_IMAGE_HASH"
+if [[ "$BUMPED_IMAGE_TAG" != "$EXPECTED_TAG" ]]; then
+  echo "Tags are not correct: wanted $EXPECTED_TAG but got $BUMPED_IMAGE_TAG"
   exit 1
 fi
 echo "Tags are correct"
