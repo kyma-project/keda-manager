ae2c959ccae0df8e61a732b30a7563839e6c4b47
Andrzej Pankowski <AndrewPank6@gmail.com>
2023-10-04 13:21:14 +0200

    change_matched_version (#293)

--- scripts/check_image.sh
+++ scripts/check_image.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-REF_NAME="${1:-"main"}"
-RAW_EXPECTED_SHA=$(git log "${REF_NAME}" --max-count 1 --skip 1 --format=format:%H)
-SHORT_EXPECTED_SHA=${RAW_EXPECTED_SHA:0:8}
-DATE="v$(git log "${REF_NAME}" --max-count 1 --skip 1 --format=format:%ad --date=format:'%Y%m%d')"
-EXPECTED_TAG="${DATE}-${SHORT_EXPECTED_SHA}"
+DESIRED_TAG=$1
 
 IMAGE_TO_CHECK="${2:-europe-docker.pkg.dev/kyma-project/prod/keda-manager}"
 BUMPED_IMAGE_TAG=$(cat sec-scanners-config.yaml | grep "${IMAGE_TO_CHECK}" | cut -d : -f 2)
 
-if [[ "$BUMPED_IMAGE_TAG" != "$EXPECTED_TAG" ]]; then
-  echo "Tags are not correct: wanted $EXPECTED_TAG but got $BUMPED_IMAGE_TAG"
+if [[ "$BUMPED_IMAGE_TAG" != "$DESIRED_TAG" ]]; then
+  echo "Tags are not correct: wanted $DESIRED_TAG but got $BUMPED_IMAGE_TAG"
   exit 1
 fi
 echo "Tags are correct"
