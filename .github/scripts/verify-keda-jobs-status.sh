e36534fb06082671a1ee7adb583714a5b2097f94
Andrzej Pankowski <andrzej.pankowski@sap.com>
2023-12-07 13:41:44 +0100

    review

--- .github/scripts/verify-keda-jobs-status.sh
+++ .github/scripts/verify-keda-jobs-status.sh
@@ -10,7 +10,7 @@ STATUS_URL="https://api.github.com/repos/kyma-project/keda-manager/commits/${REF
 function verify_github_jobs_status () {
 	local number=1
 	while [[ $number -le 100 ]] ; do
-		echo ">--> checking keda release job status #$number"
+		echo ">--> checking keda job status #$number"
 		local STATUS=`curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" ${STATUS_URL} | jq -r .state `
 		echo "jobs status: ${STATUS:='UNKNOWN'}"
 		[[ "$STATUS" == "success" ]] && return 0
