ceb01eb6029bf4098d9f108f2307c87393694209
Andrzej Pankowski <andrzej.pankowski@sap.com>
2023-12-07 13:35:28 +0100

    unify_scripts

--- .github/scripts/verify-keda-jobs-status.sh
+++ .github/scripts/verify-keda-jobs-status.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-
+sleep 10
 
 echo "Checking status of POST Jobs for Keda-Manager"
 
 REF_NAME="${1:-"main"}"
 STATUS_URL="https://api.github.com/repos/kyma-project/keda-manager/commits/${REF_NAME}/status"
 
-function get_keda_status () {
+function verify_github_jobs_status () {
 	local number=1
 	while [[ $number -le 100 ]] ; do
 		echo ">--> checking keda release job status #$number"
-		local STATUS=`curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" ${STATUS_URL} | head -n 2 `
+		local STATUS=`curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" ${STATUS_URL} | jq -r .state `
 		echo "jobs status: ${STATUS:='UNKNOWN'}"
-		[[ "$STATUS" == *"Success"* ]] && return 0
+		[[ "$STATUS" == "success" ]] && return 0
+		[[ "$STATUS" == "failure" ]] && return 1
 		sleep 5
         	((number = number + 1))
 	done
@@ -21,4 +22,4 @@ function get_keda_status () {
 	exit 1
 }
 
-get_kyma_status
+verify_github_jobs_status
