8af1f098ef8509612eb758feb3bcf34760b7e8bb
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2024-03-15 14:19:58 +0100

    Bump Keda to 2.13.1

--- controllers/keda_controller_rbacs.go
+++ controllers/keda_controller_rbacs.go
@@ -5,6 +5,7 @@ package controllers
 //+kubebuilder:rbac:groups="",resources=configmaps;configmaps/status;events;services,verbs="*"
 //+kubebuilder:rbac:groups="",resources=external;pods;secrets;serviceaccounts,verbs=list;watch;create;delete;update;patch
 //+kubebuilder:rbac:groups="",resources=namespaces,verbs=create;delete
+//+kubebuilder:rbac:groups="",resources=limitranges,verbs=list;watch
 //+kubebuilder:rbac:groups=apiregistration.k8s.io,resources=apiservices,verbs=create;delete;update;patch;watch;list
 //+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterrolebindings;clusterroles;rolebindings;roles,verbs=create;delete;update;patch;watch;list
 //+kubebuilder:rbac:groups=authorization.k8s.io,resources=subjectaccessreviews,verbs=create
@@ -16,6 +17,8 @@ package controllers
 //+kubebuilder:rbac:groups=batch,resources=jobs,verbs="*"
 //+kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs="*"
 //+kubebuilder:rbac:groups="keda.sh",resources=clustertriggerauthentications;clustertriggerauthentications/status;scaledjobs;scaledjobs/finalizers;scaledjobs/status;scaledobjects;scaledobjects/finalizers;scaledobjects/status;triggerauthentications;triggerauthentications/status,verbs="*"
+//+kubebuilder:rbac:groups="eventing.keda.sh",resources=cloudeventsources;cloudeventsources/status,verbs="*"
+
 //+kubebuilder:rbac:groups=autoscaling,resources=horizontalpodautoscalers,verbs="*"
 //+kubebuilder:rbac:groups=admissionregistration.k8s.io,resources=validatingwebhookconfigurations,verbs=get;create;list;patch;update;watch;delete
 
