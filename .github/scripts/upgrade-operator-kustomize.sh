6111f68e5936b475d8c0f0a8aa1de2925c48d6fd
Filip Strozik <filip.strozik@outlook.com>
2025-02-05 13:55:16 +0100

    Adjust kustomize labels injection mechanism (#575)
    * format yaml
    
    * upgrade img and version as it is in other otters repos
    
    * do not make new manifests

---
+++ .github/scripts/upgrade-operator-kustomize.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# standard bash error handling
+set -o errexit  # exit immediately when a command fails.
+set -E          # needs to be set if we want the ERR trap
+set -o pipefail # prevents errors in a pipeline from being masked
+
+# Expected variables:
+IMG=${IMG?"Define IMG env"} # operator img
+MODULE_VERSION=${MODULE_VERSION?"Define MODULE_VERSION env"} # module version used to set common labels
+
+PROJECT_ROOT=${PWD}
+CONFIG_OPERATOR=${PROJECT_ROOT}/config
+
+echo "ensure kustomize..."
+PROJECT_ROOT=${PROJECT_ROOT} make -C ${PROJECT_ROOT} kustomize
+
+echo "upgrade ${CONFIG_OPERATOR}..."
+
+echo "upgrade image to ${IMG}..."
+cd ${CONFIG_OPERATOR}/manager && ${PROJECT_ROOT}/bin/kustomize edit set image controller=${IMG}
+
+echo "upgrade module version to ${MODULE_VERSION}..."
+cd ${CONFIG_OPERATOR}/default && ${PROJECT_ROOT}/bin/kustomize edit add label app.kubernetes.io/version:${MODULE_VERSION} --force --without-selector --include-templates
--- Makefile
+++ Makefile
@@ -93,8 +93,7 @@ deploy: manifests kustomize ## Deploy controller to the K8s cluster specified in
 	$(KUSTOMIZE) build config/default | kubectl apply -f -
 
 .PHONY: render-manifest
-render-manifest: manifests kustomize ## Render keda-manager.yaml manifest.
-	cd config/manager && $(KUSTOMIZE) edit set image controller=${IMG}
+render-manifest: kustomize ## Render keda-manager.yaml manifest.
 	$(KUSTOMIZE) build config/default > keda-manager.yaml
 
 .PHONY: undeploy
