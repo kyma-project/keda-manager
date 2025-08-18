7a5f8a9fd5857a3f21feaf61eade5eee5b86872f
kwiatekus <krzysztof.kwiatosz@sap.com>
2025-08-18 15:47:30 +0200

    Fix img tag updating script

--- .github/scripts/upgrade-sec-scanners-config.sh
+++ .github/scripts/upgrade-sec-scanners-config.sh
@@ -2,4 +2,4 @@
 
 IMG_VERSION=${IMG_VERSION?"Define IMG_VERSION env"}
 
-yq -i ".bdba[] |= sub(\":.*\", \":${IMG_VERSION}\")" sec-scanners-config.yaml
+yq -i ".bdba[] |= sub(\"europe-docker.pkg.dev/kyma-project/prod/keda-manager:.*\", \"europe-docker.pkg.dev/kyma-project/prod/keda-manager:${IMG_VERSION}\")" sec-scanners-config.yaml
