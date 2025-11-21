f2af0026106f97e822f823144e504366bfddaf88
Michał Kalke <56382792+MichalKalke@users.noreply.github.com>
2025-11-21 13:40:32 +0100

    Update controllers/keda_controller.go
    Co-authored-by: Krzysztof Kwiatosz <krzysztof.kwiatosz@sap.com>

--- controllers/keda_controller.go
+++ controllers/keda_controller.go
@@ -119,7 +119,7 @@ func (r *kedaReconciler) SetupWithManager(mgr ctrl.Manager) error {
 			DeleteFunc: r.retriggerAllKedaCRs,
 		})
 
-	// create function to register wached objects
+	// create function to register watched objects
 	watchFn := func(u unstructured.Unstructured) {
 		r.log.With("gvk", u.GroupVersionKind().String()).Infoln("adding watcher")
 		b = b.Watches(
