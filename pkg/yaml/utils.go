c441352c29cdd8aeca8d4fd7218eadd9e28aaa1e
Cortey <andrzej.pankowski@sap.com>
2026-04-20 08:32:51 +0200

    panics

--- pkg/yaml/utils.go
+++ pkg/yaml/utils.go
@@ -1,11 +1,27 @@
 package yaml
 
 import (
+	"encoding/json"
 	"io"
 
 	"gopkg.in/yaml.v3"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
+
+// normalizeToJSON round-trips a value through JSON encoding/decoding so that
+// all numeric types become float64 (JSON-compatible), avoiding panics in
+// k8s.io/apimachinery's DeepCopyJSONValue which does not handle Go int.
+func normalizeToJSON(v map[string]interface{}) (map[string]interface{}, error) {
+	b, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+	var out map[string]interface{}
+	if err := json.Unmarshal(b, &out); err != nil {
+		return nil, err
+	}
+	return out, nil
+}
 
 func LoadData(r io.Reader) ([]unstructured.Unstructured, error) {
 	results := make([]unstructured.Unstructured, 0)
@@ -26,6 +42,13 @@ func LoadData(r io.Reader) ([]unstructured.Unstructured, error) {
 		// Skip empty documents (e.g. blank --- separators in multi-doc YAML).
 		if len(obj) == 0 {
 			continue
+		}
+
+		// Normalize numeric types to JSON-compatible float64 to avoid panics
+		// in k8s.io/apimachinery's DeepCopyJSONValue (which doesn't handle int).
+		obj, err = normalizeToJSON(obj)
+		if err != nil {
+			return nil, err
 		}
 
 		u := unstructured.Unstructured{Object: obj}
