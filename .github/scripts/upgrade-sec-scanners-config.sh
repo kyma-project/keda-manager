e83b89214f3e27df04964bc2f15b362dfd613d76
Piotr Halama <piotr.halama@sap.com>
2026-05-20 15:22:51 +0200

    Use component-config for security scans (#910)

--- .github/scripts/upgrade-sec-scanners-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-IMG_VERSION=${IMG_VERSION?"Define IMG_VERSION env"}
-
-yq -i ".bdba[] |= select(contains(\"external\") | not) |= sub(\":.*\", \":${IMG_VERSION}\")" sec-scanners-config.yaml
